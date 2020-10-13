30c40389d4af2105b50abbf48ddc37ff93ece1d2
harshit031196 <guptaharshit0311@gmail.com>
2020-10-13 17:14:28 +0530

    This is the change I wanted to make

---
+++ src/com/learn/LearnGithub.java
@@ -0,0 +1,10 @@
+package com.learn;
+
+public class LearnGithub {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("This is the learning file");
+	}
+
+}
